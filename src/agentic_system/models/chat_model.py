2a05db5c3e377dc611c5fc1625b597a05eb4ad97
Henry Li <henry1943@163.com>
2025-07-14 15:08:37 +0800

    feat: add models

---
+++ src/agentic_system/models/chat_model.py
@@ -0,0 +1,33 @@
+import os
+
+from langchain_openai import ChatOpenAI
+
+
+def create_ark_model(model: str = "doubao-1-5-pro-32k-250115") -> ChatOpenAI:
+    return ChatOpenAI(
+        model=model,
+        base_url="https://ark-cn-beijing.bytedance.net/api/v3",
+        api_key=os.getenv("DOUBAO_API_KEY"),
+        temperature=0,
+        top_p=0,
+    )
+
+
+def create_gpt4o_model(model: str = "gpt-4o-2024-11-20") -> ChatOpenAI:
+    return ChatOpenAI(
+        model=model,
+        base_url="https://search.bytedance.net/gpt/openapi/online/v2/crawl/openai/deployments",
+        default_query={"api-version": "2023-03-15-preview"},
+        default_headers={"api-key": os.getenv("GPT_OPEN_API_KEY"), "caller": "sxy"},
+        temperature=0,
+        top_p=0,
+    )
+
+
+def create_chat_model() -> ChatOpenAI:
+    return create_gpt4o_model()
+
+
+if __name__ == "__main__":
+    chat_model = create_chat_model()
+    print(chat_model.invoke("你好，豆包").content)
