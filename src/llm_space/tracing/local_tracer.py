4b30178478310def636f495f8223e9719ff184e7
Henry Li <henry1943@163.com>
2025-07-15 18:32:48 +0800

    feat: trace response as well

--- src/llm_space/tracing/local_tracer.py
+++ src/llm_space/tracing/local_tracer.py
@@ -1,8 +1,9 @@
 import json
 import os
+from uuid import UUID
 
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain.schema import BaseMessage
+from langchain.schema import BaseMessage, LLMResult
 from langchain_core.messages import convert_to_openai_messages
 
 
@@ -14,12 +15,13 @@ class LocalTracer(BaseCallbackHandler):
         self._storage_path = os.path.abspath(storage_path)
         os.makedirs(self._storage_path, exist_ok=True)
 
-    def next_id(self):
-        self.id_counter += 1
-        return self.id_counter
-
     def on_chat_model_start(
-        self, serialized: dict, message_lists: list[list[BaseMessage]], **kwargs
+        self,
+        serialized: dict,
+        message_lists: list[list[BaseMessage]],
+        *,
+        run_id: UUID,
+        **kwargs,
     ):
         params: dict = kwargs["invocation_params"]
         for message_list in message_lists:
@@ -27,13 +29,35 @@ class LocalTracer(BaseCallbackHandler):
             chat_completion_params["messages"] = convert_to_openai_messages(
                 message_list
             )
-            self.log_chat_completion(chat_completion_params)
+            self.log_chat_completion(run_id, chat_completion_params)
 
-    def log_chat_completion(self, chat_completion_params: dict):
+    def on_llm_end(
+        self,
+        response: LLMResult,
+        *,
+        run_id: UUID,
+        **kwargs,
+    ):
+        message = response.generations[0][0].message
+        converted_message = convert_to_openai_messages([message])[0]
+        self.update_chat_completion_response(run_id, converted_message)
+
+    def log_chat_completion(self, run_id: UUID, data: dict):
         file_name = os.path.join(
             self._storage_path,
-            f"chat-completion-{self.next_id():03d}.json",
+            f"run-{run_id}.json",
         )
         with open(file_name, "w") as f:
-            json.dump(chat_completion_params, f, indent=2, ensure_ascii=False)
-        print(f"********** Saved in {file_name} **********\n\n\n\n")
+            json.dump(data, f, indent=2, ensure_ascii=False)
+        print(f'Saved in "{file_name}"')
+
+    def update_chat_completion_response(self, run_id: UUID, response_message: dict):
+        file_name = os.path.join(
+            self._storage_path,
+            f"run-{run_id}.json",
+        )
+        with open(file_name, "r") as f:
+            existing_data = json.load(f)
+        existing_data["messages"].append(response_message)
+        with open(file_name, "w") as f:
+            json.dump(existing_data, f, indent=2, ensure_ascii=False)
